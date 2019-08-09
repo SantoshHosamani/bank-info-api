7a7ddb630ee3f9903bc9343b68109f84d239b3d3
SantoshHosamani <53813843+SantoshHosamani@users.noreply.github.com>
2019-08-09 22:51:07 +0530

    Add files via upload

--- bank/api/views.py
+++ bank/api/views.py
@@ -32,25 +32,33 @@ class BranchInfo(APIView):
         serializer = Serializer(branch_info, many=True)
         return JsonResponse(serializer.data,safe=False)
 
-def Read(request):
-    # Add CSV file Path 
-    csv_file = '/home/secpod/Downloads/branch.csv'
-    reader = csv.DictReader(open(csv_file))
-    for row in reader:
-        bank_name = row.get('bank_name')
-        ifsc = row.get('ifsc')
-        branch = row.get('branch')
-        address = row.get('address')
-        city = row.get('city')
-        district = row.get('district')
-        state = row.get('state')
-        string=''        
-        if ifsc:
-            bank_object, created = Bank.objects.get_or_create(name=bank_name)
-            branch_info = { 'name': branch,'bank': bank_object,'address': address,'city': city,'district': district,'state': state }    
-            branch_object, created = Branch.objects.update_or_create(ifsc=ifsc, defaults=branch_info)
 
-    return HttpResponse('Done')
+class Read(APIView):
+    permission_classes = (IsAuthenticated,) 
+
+    def get(self, request):
+        return render(request, 'upload_csv.html')
+
+    def post(self, request):
+        csv_file = request.FILES.get('csv_file')
+        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        #csv_file = 'C:/Users/muttu1/Desktop/Book.csv'
+        reader = csv.DictReader(open(decoded_file))
+        for row in reader:
+            bank_name = row.get('bank_name')
+            ifsc = row.get('ifsc')
+            branch = row.get('branch')
+            address = row.get('address')
+            city = row.get('city')
+            district = row.get('district')
+            state = row.get('state')
+            string=''        
+            if ifsc:
+                bank_object, created = Bank.objects.get_or_create(name=bank_name)
+                branch_info = { 'name': branch,'bank': bank_object,'address': address,'city': city,'district': district,'state': state }    
+                branch_object, created = Branch.objects.update_or_create(ifsc=ifsc, defaults=branch_info)
+
+        return HttpResponse('Done')
 
 
 
